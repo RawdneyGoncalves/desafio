dadbe221885df5e72d2c571d2cc76e6f4cedcf79
Rawdney Mendes <mendesrawdney@gmail.com>
2024-07-20 02:09:02 -0300

    git commit -m "feat: initialize project with TypeScript and Express"

---
+++ src/config/database.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { Package } from '../entities/package.entity';
+import { Theme } from '../entities/theme.entity';
+import { Movie } from '../entities/movie.entity';
+
+const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
+  logging: false,
+  entities: [User, Package, Theme, Movie],
+  migrations: [],
+  subscribers: [],
+});
+
+export default AppDataSource;
